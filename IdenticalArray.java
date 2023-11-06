063c48c3d96604ecc2def31465b2fa09df6c443c
ZakPug <51068330+ZakPug@users.noreply.github.com>
2023-11-06 04:18:05 +0000

    Initial commit

---
+++ IdenticalArray.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class IdenticalArray {
+
+    public static void main(String[] args) {
+        // Prompt the user to enter two 3x3 arrays
+        System.out.println("Enter the first 3x3 array:");
+        int[][] m1 = enterMatrix();
+
+        System.out.println("Enter the second 3x3 array:");
+        int[][] m2 = enterMatrix();
+
+        // Check if the arrays are identical and display the result
+        if (areIdentical(m1, m2)) {
+            System.out.println("The two arrays are identical.");
+        } else {
+            System.out.println("The two arrays are not identical.");
+        }
+    }
+
+    public static boolean areIdentical(int[][] m1, int[][] m2) {
+        if (m1.length != m2.length || m1[0].length != m2[0].length) {
+            return false; // Arrays have different dimensions, so they can't be identical.
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (m1[i][j] != m2[i][j]) {
+                    return false; // Arrays have different elements, so they can't be identical.
+                }
+            }
+        }
+
+        return true; // Arrays are identical.
+    }
+
+    public static int[][] enterMatrix() {
+        Scanner input = new Scanner(System.in);
+        int[][] matrix = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Enter element at position (" + i + ", " + j + "): ");
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        return matrix;
+    }
+}
